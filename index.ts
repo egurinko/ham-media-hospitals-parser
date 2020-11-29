014c2b6109be3ab246dd0ea6715dbab975e3e6da
egurinko <egctoru@gmail.com>
2020-11-29 22:33:29 +0900

    include everything

--- index.ts
+++ index.ts
@@ -4,19 +4,29 @@ import csv from "csv-parser";
 fs.readdir("hospitals", (error, fileNames) => {
     const hospitals: any = {};
 
-    fileNames.map((fileName, index)=>{
+    const fileReadPromises = fileNames.map((fileName, index)=>{
         const prefectureHospitals: any = [];
-        fs.createReadStream(`./hospitals/${fileName}`)
-            .pipe(csv())
-            .on("data", (data) => prefectureHospitals.push(data))
-            .on("end", ()=>{
-                const prefectureName = fileName.split(".")[0];
-                hospitals[prefectureName] = prefectureHospitals;
 
-                if (index === fileNames.length-1) {
-                    const jsonHospitals = JSON.stringify(hospitals);
-                    fs.writeFileSync('hospitals.json', jsonHospitals);
-                }
-            })
+        return new Promise((resolve, reject)=>{
+            fs.createReadStream(`./hospitals/${fileName}`)
+                            .pipe(csv())
+                            .on("data", (data) => prefectureHospitals.push(data))
+                            .on("end", ()=>{
+                                const prefectureName = fileName.split(".")[0];
+                                hospitals[prefectureName] = prefectureHospitals;
+
+                                resolve("read");
+                            })
+        })
     })
+
+    Promise
+        .all(fileReadPromises)
+        .then(()=>{
+            const jsonHospitals = JSON.stringify(hospitals);
+            fs.writeFileSync('hospitals.json', jsonHospitals);
+
+            console.log("FINISH:", Object.keys(hospitals).length);
+            console.log("道府県 + 東京23区 + 東京23区外");
+        })
 })
